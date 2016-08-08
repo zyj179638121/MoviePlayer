a3651557be515a4bd0538eebfde995e5a9d8c11a
zhaoyongjie <18515381796@163.com>
2016-08-08 11:07:14 +0800

    Initial Commit

---
+++ XZLive/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XZLive
+//
+//  Created by MYKJ on 16/8/8.
+//  Copyright © 2016年 ZYJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ XZLiveTests/XZLiveTests.swift
@@ -0,0 +1,36 @@
+//
+//  XZLiveTests.swift
+//  XZLiveTests
+//
+//  Created by MYKJ on 16/8/8.
+//  Copyright © 2016年 ZYJ. All rights reserved.
+//
+
+import XCTest
+@testable import XZLive
+
+class XZLiveTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
